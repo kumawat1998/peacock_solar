e34be7456ec99636a5f730f34613c5a9baeaf666
kumawat1998 <nemiku1998@gmail.com>
2020-05-17 13:54:14 +0530

    email validation unit test

--- app/src/main/java/com/example/mycode/MainActivity.java
+++ app/src/main/java/com/example/mycode/MainActivity.java
@@ -10,10 +10,12 @@ import android.widget.Toast;
 
 import com.example.mycode.formclass.User_form;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     EditText emailAddress,name,address;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,21 +29,33 @@ public class MainActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               if(!validateEmail(emailAddress)|
+               if(!validateEmail(emailAddress.getText().toString().trim())|
                 !validateName(name)|
-                !validateAddress(address)) {
+                       !validateAddress(address)) {
                    Toast.makeText(MainActivity.this, "Form is not submitted", Toast.LENGTH_SHORT).show();
                    return;
                }
                User_form form=new User_form(emailAddress.getText().toString().trim(),name.getText().toString().trim(),address.getText().toString().trim());
                 Toast.makeText(MainActivity.this, name.getText().toString().trim()+"\n"+emailAddress.getText().toString().trim()+"\n"+address.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+
             }
         });
 
     }
-   private boolean validateEmail( EditText emailAddress){
-        String emailInput=emailAddress.getText().toString().trim();
-
+    public boolean emailvalidationtest( final String emailInput)
+    {
+        final String regex1="^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";
+        Pattern pattern;
+        Matcher matcher;
+        pattern=Pattern.compile(regex1);
+        if(emailInput.isEmpty())
+        {
+            return false;
+        }
+        matcher=pattern.matcher(emailInput);
+        return matcher.matches();
+    }
+    private boolean validateEmail(String emailInput ){
         if(emailInput.isEmpty()){
             emailAddress.setError("Field can't be empty");
             return false ;
@@ -78,4 +92,6 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     }
+
 }
+
